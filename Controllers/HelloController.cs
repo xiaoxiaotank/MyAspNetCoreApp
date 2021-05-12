0c3a92875d2b1cac1125d6c9a593aba2158e5bbe
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2021-05-12 11:17:32 +0800

    init

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAspNetCoreApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private readonly ILogger<HelloController> _logger;
+
+        public HelloController(ILogger<HelloController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return $"{DateTime.Now:o}: Hello !";
+        }
+
+        [HttpGet("{name}")]
+        public string Get(string name)
+        {
+            return $"{DateTime.Now:o}: Hello {name} !";
+        }
+    }
+}
